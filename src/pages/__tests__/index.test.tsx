47ff388b9eaadd4800a97df9c05e6c69447932e9
Ali <ali.hussein.pre@gmail.com>
2021-06-15 11:23:05 +0200

    test(testing files): remove unnecessary testing files

--- src/pages/__tests__/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import HomePage from '@/pages/index';
-import { render } from 'src/testUtils/testUtils';
-
-describe('Home page', () => {
-  it('Should  WHEN ', () => {
-    const { container } = render(<HomePage />);
-    expect(container).toBeDefined();
-  });
-});
