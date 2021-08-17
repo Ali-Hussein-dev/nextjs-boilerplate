fac2260b460f6f9268305226699b1dab17ee8165
Ali <ali.hussein.pre@gmail.com>
2021-08-17 11:22:02 +0200

    chore(tailwindcss): install plugins

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,21 @@
+import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import '../css/global.css';
-import { extendTheme } from '@chakra-ui/react';
+import { theme } from '@chakra-ui/react';
+const devMode = process.env.screen_breakpoint;
 
-const theme = extendTheme({
-  styles: {
-    global: {
-      body: {
-        fontFamily: 'Poppins, Sans-serif',
-      },
-    },
-  },
-});
 const MyApp: React.FC<{ Component: React.FC; pageProps: any }> = ({
   Component,
   pageProps,
 }): JSX.Element => {
   return (
-    <ChakraProvider theme={theme}>
-      <link rel="icon" href="/favicon.ico" />
-      <title>Nextjs-boilerplate</title>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <div className={`${devMode ? 'debug-screens' : ''} bg-hero-polka-dots-100`}>
+      <ChakraProvider theme={theme}>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Nextjs-boilerplate</title>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,51 @@ module.exports = {
       colors: {
         blueGray: colors.blueGray,
         trueGray: colors.trueGray,
-        lightBlue: colors.lightBlue,
+        sky: colors.sky,
         orange: colors.orange,
       },
+    },
+    debugScreens: {
+      style: {
+        backgroundColor: '#eab308',
+        color: '#334155',
+      },
+      position: ['top', 'left'],
     },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
+    require('tailwindcss-debug-screens'),
+    require('tailwindcss-scroll-snap'),
+    require('tailwind-heropatterns')({
+      // as per tailwind docs you can pass variants
+      variants: [],
+
+      // the list of patterns you want to generate a class for
+      // the names must be in kebab-case
+      // an empty array will generate all 87 patterns
+      patterns: [],
+
+      // The foreground colors of the pattern
+      colors: {
+        default: '#d1d5db',
+        'blue-dark': '#000044', //also works with rgb(0,0,205)
+      },
+      // The foreground opacity
+      opacity: {
+        default: '0.4',
+        10: '0.1',
+        20: '0.2',
+        30: '0.3',
+        50: '0.5',
+        60: '0.6',
+        70: '0.7',
+        80: '0.8',
+        90: '0.9',
+        100: '1.0',
+      },
+    }),
   ],
+  variants: {},
 };
